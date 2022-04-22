cd99fb3ec3310e0dddedc7018b0d2bd76a40d99d
Jeonghoon Lee <hoon22.lee@gmail.com>
2022-04-22 14:45:01 -0400

    database script

---
+++ bulls_and_cows_db.sql
@@ -0,0 +1,27 @@
+drop database if exists guessNumberDB;
+create database guessNumberDB;
+
+use guessNumberDB;
+
+create table game (
+	gameId int auto_increment primary key,
+	answer int not null,
+    status tinyint default(0)
+)
+;
+
+create table round (
+	roundId int auto_increment primary key,
+    guess int not null,
+    gameId int not null,
+    --
+    constraint fk_game_round foreign key (gameId) references game(gameId)
+)
+;
+
+-- for testing
+-- insert into game (gameId, answer) values (1, 1234);
+-- select * from game;
+
+-- insert into round (guess, gameId) values (1234, 1);
+-- select * from round;
